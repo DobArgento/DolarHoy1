e7a6fa6b5c79714f1b9c6fa12b6c24024ab01995
DobArgento <toti300607@gmail.com>
2024-02-06 22:47:01 -0300

    Corrección de errores

--- app.js
+++ app.js
@@ -12,14 +12,24 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch(error => console.error('Error al obtener los datos:', error));
 });
 
-function mostrarTasasDeCambio(tasas) {
+function mostrarTasasDeCambio(_tasas_) {
+
     // Actualiza el código según la estructura de datos de la API criptoya.com
-    document.getElementById("dolarBlueAsk").innerHTML = `<strong>Dólar Blue (Venta):</strong> $${tasas.blue.ask}`;
-    document.getElementById("dolarBlueBid").innerHTML = `<strong>Dólar Blue (Compra):</strong> $${tasas.blue.bid}`;
-    document.getElementById("criptoCCBAsk").innerHTML = `<strong>Cripto CCB (Venta):</strong> $${tasas.cripto.ccb.ask}`;
-    document.getElementById("criptoCCBBid").innerHTML = `<strong>Cripto CCB (Compra):</strong> $${tasas.cripto.ccb.bid}`;
-    document.getElementById("mepAL30").innerHTML = `<strong>Dólar MEP:</strong> $${tasas.mep.al30["48hs"].price}`;
-    document.getElementById("mepCI").innerHTML = `<strong>MEP CI:</strong> $${tasas.mep.ci.price}`;
-    document.getElementById("cclAL30").innerHTML = `<strong>CCL AL30:</strong> $${tasas.ccl.al30["48hs"].price}`;
-    document.getElementById("cclCI").innerHTML = `<strong>CCL CI:</strong> $${tasas.ccl.ci.price}`;
+
+    document.getElementById("dolarBlueAsk").innerHTML = `<strong>Dólar Blue (Venta):</strong> $${_tasas_.blue.ask}`;
+
+    document.getElementById("dolarBlueBid").innerHTML = `<strong>Dólar Blue (Compra):</strong> $${_tasas_.blue.bid}`;
+
+    document.getElementById("criptoCCBAsk").innerHTML = `<strong>Cripto CCB (Venta):</strong> $${_tasas_.cripto.ccb.ask}`;
+
+    document.getElementById("criptoCCBBid").innerHTML = `<strong>Cripto CCB (Compra):</strong> $${_tasas_.cripto.ccb.bid}`;
+
+    document.getElementById("mepAL30").innerHTML = `<strong>Dólar MEP:</strong> $${_tasas_.mep.al30["48hs"].price}`;
+
+    document.getElementById("mepCI").innerHTML = `<strong>MEP CI:</strong> $${_tasas_.mep.al30.ci.price}`;
+
+    document.getElementById("cclAL30").innerHTML = `<strong>CCL AL30:</strong> $${_tasas_.ccl.al30["48hs"].price}`;
+
+    document.getElementById("cclCI").innerHTML = `<strong>CCL CI:</strong> $${_tasas_.ccl.al30.ci.price}`;
+
 }
